ed3c0e69c754383c05ae767c08585975da01bc98
lina-22 <haquefahinur@gmail.com>
2023-12-09 20:47:42 +0100

    student service updated

--- student-management-system/src/main/java/com/studentmanagementsystem/service/StudentService.java
+++ student-management-system/src/main/java/com/studentmanagementsystem/service/StudentService.java
@@ -5,4 +5,8 @@ import java.util.List;
 public interface StudentService {
     List<Student>getAllStudent();
     Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+    Student updateStudent(Student student);
+
 }
